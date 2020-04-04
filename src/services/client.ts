92e7ca2c356b844cb1153f9d12fe975063fd7810
Roomy <cilpong@gmail.com>
2020-04-04 15:32:33 +0900

    Delete: console.log promise error in development

--- src/services/client.ts
+++ src/services/client.ts
@@ -1,18 +1,16 @@
 import axios, { AxiosError } from "axios";
 
 const client = axios.create({
-  timeout: 6000
+  timeout: 6000,
 });
 
 client.interceptors.response.use(
-  response => {
+  (response) => {
     const { data } = response;
     return data;
   },
   (error: AxiosError) => {
     const { response: errorData } = error;
-
-    if (process.env.APP_ENV === "development") console.error(errorData);
 
     return new Promise((resolve, reject) => reject(errorData));
   }
